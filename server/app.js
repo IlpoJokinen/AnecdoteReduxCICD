5019cd53ddb35c138947a10a4164833676071943
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2021-04-09 10:42:52 +0300

    Continued implementing the deployment pipeline

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const anecdoteRouter = require('./routers/anecdoteRouter');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const cors = require('cors');
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  // eslint-disable-next-line no-unused-vars
+  .then(result => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/anecdotes', anecdoteRouter);
+
+app.use('/api/health', (request, response) => {
+  response.send('ok');
+})
+
+module.exports = app;
